44b630c7c7c34265aeb61daeb12121b91d619d3b
AnastasiaRiabova <riabova321@gmail.com>
2023-10-15 14:44:36 +0200

    add project tasks

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,36 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const galleryContainerRef = document.querySelector(".gallery");
+
+let lightBoxInstance
+const handleImageClick = (e) => {
+  e.preventDefault()
+  const handleEscape = (e) => {
+    if (e.key === 'Escape') lightBoxInstance.close()
+  }
+  lightBoxInstance = basicLightbox.create(`
+      <img alt="image" src="${e.target.dataset.source}" width="auto" height="auto">
+  `, {
+    onShow: () => { document.addEventListener('keydown', handleEscape) },
+    onClose: () => { document.removeEventListener('keydown', handleEscape) },
+  })
+  lightBoxInstance.show()
+}
+
+galleryContainerRef.addEventListener('click', handleImageClick)
+const createMarkup = (ref) => {
+  const images = galleryItems.map(({ preview, original, description }) => {
+    return `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+       class="gallery__image"
+       src="${preview}"
+       data-source="${original}"
+       alt="${description}"
+      />
+    </a>
+  </li>`}).join('')
+
+  ref.insertAdjacentHTML("afterbegin", images)
+}
+createMarkup(galleryContainerRef)
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryContainerRef = document.querySelector('.gallery');
+galleryContainerRef.addEventListener('click', (e) => {
+  e.preventDefault()
+})
+const createMarkup = (ref) => {
+  const images = galleryItems.map(({ preview, original, description }, ind) => {
+    return `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+       class="gallery__image"
+       src="${preview}"
+       alt="${description}"
+      />
+    </a>
+  </li>`}).join('')
+
+  ref.insertAdjacentHTML("afterbegin", images)
+}
+createMarkup(galleryContainerRef)
+
+new SimpleLightbox('.gallery__link', {
+  captions: true,
+  captionsData: "alt",
+  captionDelay: 250,
+});
